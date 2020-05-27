f0a2187ebded08361d497aa5535c03e7c819ad12
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-05-27 15:24:19 -0400

    Updates

---
+++ precache-manifest.b0869c3bdfb92a24565a7e2224a8a80e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14c6b2fcab3bd42b39513fde877dcf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199806094943d33c8751",
+    "url": "/static/css/main.a5652947.chunk.css"
+  },
+  {
+    "revision": "14083c19042ae316866f",
+    "url": "/static/js/2.2f619050.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.2f619050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199806094943d33c8751",
+    "url": "/static/js/main.b308d303.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  },
+  {
+    "revision": "99d1f784880c990ea8d4c5dd3b320589",
+    "url": "/static/media/budget.99d1f784.png"
+  },
+  {
+    "revision": "7fe1c44ec8c1249459f68a930ded48ad",
+    "url": "/static/media/coffee.7fe1c44e.png"
+  },
+  {
+    "revision": "34ef1b5fe126f2250a9dbfdfca0209d1",
+    "url": "/static/media/etchasketch.34ef1b5f.png"
+  },
+  {
+    "revision": "73c3b34a2d73ea3d1a852d549a8e4177",
+    "url": "/static/media/events.73c3b34a.png"
+  },
+  {
+    "revision": "8410779f730ce6fb36fa16700d76b9b1",
+    "url": "/static/media/gallery.8410779f.png"
+  },
+  {
+    "revision": "bc06747ad740717233ebc6c05b92f55f",
+    "url": "/static/media/games-coming-soon.bc06747a.png"
+  },
+  {
+    "revision": "6ecabc897d9cd3218e7b16184ba4f27b",
+    "url": "/static/media/gametracker-ver1-sinatra.6ecabc89.png"
+  },
+  {
+    "revision": "e552b211018607e455b97308b353432c",
+    "url": "/static/media/gametracker-ver2-rails.e552b211.png"
+  },
+  {
+    "revision": "72a56a60b1385893cbb5fd5aa2d3aca1",
+    "url": "/static/media/gametracker-ver2-rails02.72a56a60.png"
+  },
+  {
+    "revision": "a22d51c44920d8c3915d97a7361d5a65",
+    "url": "/static/media/hoosthere.a22d51c4.png"
+  },
+  {
+    "revision": "1432da4098512fa9f4c1139e7da9dc34",
+    "url": "/static/media/insult_swordfighting.1432da40.png"
+  },
+  {
+    "revision": "4ff930206c7c84989a59e02966844513",
+    "url": "/static/media/invasion.4ff93020.png"
+  },
+  {
+    "revision": "c86ed079be8fffa27f46d4478d24ccc0",
+    "url": "/static/media/kana.c86ed079.png"
+  },
+  {
+    "revision": "2ff2f3f5d54fe337362404b95716a3b8",
+    "url": "/static/media/library.2ff2f3f5.png"
+  },
+  {
+    "revision": "fd97e4d2d1ae590cfbc00f53c27bc63c",
+    "url": "/static/media/matching.fd97e4d2.png"
+  },
+  {
+    "revision": "e4334ed985843ac3f9b9b74c09468f28",
+    "url": "/static/media/react-todo.e4334ed9.png"
+  },
+  {
+    "revision": "523a04976a31e8b7a6d069c371ffd84e",
+    "url": "/static/media/tictactoe.523a0497.png"
+  }
+]);
