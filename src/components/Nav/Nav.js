6f2305595c8431b1993b35034ae19181af528516
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-06-11 20:56:40 -0400

    add external link to blog

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ExternalLink from '../ExternalLink/ExternalLink';
 import './Nav.css';
 
 const Nav = () => (
@@ -7,6 +8,7 @@ const Nav = () => (
     <NavLink exact to='/' activeClassName='active'>About</NavLink>
     <NavLink exact to='/portfolio' activeClassName='active'>Portfolio</NavLink>
     <NavLink exact to='/contact' activeClassName='active'>Contact</NavLink>
+    <ExternalLink url="https://robotspacefish-blog.netlify.app/" text="Blog" />
   </nav>
 );
 
